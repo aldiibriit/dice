d6503186fab7ebc41656d65f0829f47555997a28
aldi <aldi.sptra86@gmail.com>
2022-08-28 11:20:29 +0700

    perbaiki beberapa hal

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 func game(jumlahDadu, jumlahPemain int) {
 	result := make(map[string][]int, jumlahPemain)
 	for i := 1; i <= jumlahPemain; i++ {
-		mapName := `hasil-lemparan-pemain-` + strconv.Itoa(i) + ``
+		mapName := `pemain-` + strconv.Itoa(i) + ``
 		result[mapName] = make([]int, jumlahDadu)
 	}
 	counter := 0
@@ -32,16 +32,37 @@ func game(jumlahDadu, jumlahPemain int) {
 			}
 		}
 		fmt.Println("========= HASIL LEMPAR DADU =========")
-		for i, _ := range result {
-			fmt.Println(i, " : ", result[i])
+		for i := 1; i <= len(result); i++ {
+			fmt.Println(`pemain-`+strconv.Itoa(i), " : ", result[`pemain-`+strconv.Itoa(i)])
 		}
 
 		fmt.Println("========= SETELAH EVALUASI =========")
-		for i, v := range result {
-			arrayBaru := evaluasiAngka6(v, make([]int, 0), i)
-			poin := jumlahDadu - len(arrayBaru)
-			response := i + ` mendapat ` + strconv.Itoa(poin) + " poin "
-			fmt.Println(response, arrayBaru)
+		exist := 0
+		for i := 1; i <= len(result); i++ {
+			arrayBaru := evaluasiAngka6(result[`pemain-`+strconv.Itoa(i)], make([]int, 0), `pemain-`+strconv.Itoa(i))
+			arrayBaru2 := evaluasiAngka1(arrayBaru, make([]int, 0), `pemain-`+strconv.Itoa(i))
+
+			if exist == i {
+				arrayBaru2 = append(arrayBaru2, 1)
+			}
+
+			if i == 1 {
+				for _, val := range result["pemain-"+strconv.Itoa(len(result))] {
+					if val == 1 {
+						arrayBaru2 = append(arrayBaru2, 1)
+					}
+				}
+			}
+
+			poin := jumlahDadu - len(arrayBaru2)
+			response := `pemain-` + strconv.Itoa(i) + ` mendapat ` + strconv.Itoa(poin) + " poin "
+			fmt.Println(response, arrayBaru2)
+
+			for _, val := range result["pemain-"+strconv.Itoa(i)] {
+				if val == 1 {
+					exist = i + 1
+				}
+			}
 		}
 
 		for i, _ := range result {
@@ -72,6 +93,15 @@ func lemparDadu(jumlahDadu int, indexName string, mapPemain map[string][]int) {
 func evaluasiAngka6(data []int, data2 []int, index string) []int {
 	for i := 0; i < len(data); i++ {
 		if data[i] != 6 && data[i] != 0 {
+			data2 = append(data2, data[i])
+		}
+	}
+	return data2
+}
+
+func evaluasiAngka1(data []int, data2 []int, index string) []int {
+	for i := 0; i < len(data); i++ {
+		if data[i] != 1 && data[i] != 0 {
 			data2 = append(data2, data[i])
 		}
 	}
